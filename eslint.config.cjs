80e8b92488db6af6e0e3bfafee58020cf05a0dff
brendanharrington <brha8770@colorado.edu>
2025-10-29 15:38:14 -0600

    chore: complete exercises 10.1-10.2
    configure directory as git repository and add eslint support

---
+++ eslint.config.cjs
@@ -0,0 +1,35 @@
+module.exports = [
+  {
+    ignores: ["node_modules/**"]
+  },
+  require("@eslint/js").configs.recommended,
+
+  require("eslint-plugin-react").configs.flat.recommended,
+
+  {
+    files: ["**/*.{js,jsx}"],
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parser: require("@babel/eslint-parser"),
+      parserOptions: {
+        requireConfigFile: false,
+        ecmaFeatures: { jsx: true }
+      },
+      globals: require("eslint-plugin-react-native").environments["react-native"].globals
+    },
+    plugins: {
+      react: require("eslint-plugin-react"),
+      "react-native": require("eslint-plugin-react-native")
+    },
+    settings: {
+      react: {
+        version: "detect"
+      }
+    },
+    rules: {
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off"
+    }
+  }
+];
