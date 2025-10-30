d1b00458905152bebaf37c33b6c202d50a6cad89
brendanharrington <brha8770@colorado.edu>
2025-10-30 14:31:01 -0600

    style: complete exercise 10.5
    improve styling of repository list by using flex boxes

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,13 +1,22 @@
-import { Text, View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 import RepositoryList from './RepositoryList';
 import AppBar from './AppBar';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#e1e4e8',
+    height: '100%',
+    padding: 10,
+  },
+});
 
 const Main = () => {
   return (
     <View>
       <AppBar />
-      <Text>Rate Repository Application</Text>
-      <RepositoryList />
+      <View style={styles.container}>
+        <RepositoryList />
+      </View>
     </View>
   );
 };
--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,15 +1,111 @@
-import { View, Text } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
+
+import Text from './Text';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'white',
+    padding: 10,
+    borderRadius: 5,
+  },
+  avatar: {
+    height: 50,
+    width: 50,
+    borderRadius: 5
+  },
+  userRow: {
+    flexDirection: 'row',
+    alignItems: 'top',
+    gap: 20,
+    marginBottom: 10,
+  },
+  userText: {
+    flexDirection: 'column',
+    gap: 10,
+  },
+  fullName: {
+    color: theme.colors.textPrimary,
+    fontSize: theme.fontSizes.subheading,
+    fontWeight: theme.fontWeights.bold,
+  },
+  description: {
+    color: theme.colors.textSecondary,
+    fontSize: theme.fontSizes.subheading,
+    fontWeight: theme.fontWeights.normal,
+  },
+  language: {
+    color: theme.colors.appBarText,
+    fontSize: theme.fontSizes.normal,
+    fontWeight: theme.fontWeights.normal,
+    backgroundColor: theme.colors.primary,
+    paddingVertical: 4,
+    paddingHorizontal: 8,
+    borderRadius: 5,
+    alignSelf: 'flex-start', // prevent stretching to full width
+  },
+  statsRow: {
+    flexDirection: 'row',
+    justifyContent: 'space-evenly',
+  },
+  statContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 10,
+    gap: 5,
+  },
+  stat: {
+    color: theme.colors.textPrimary,
+    fontSize: theme.fontSizes.subheading,
+    fontWeight: theme.fontWeights.bold,
+  },
+  statTitle: {
+    color: theme.colors.textSecondary,
+    fontSize: theme.fontSizes.subheading,
+    fontWeight: theme.fontWeights.normal,
+  }
+})
 
 const RepositoryItem = ({ item }) => {
+  const formatStat = stat => {
+    if (stat < 1000) return stat;
+
+    return (stat / 1000).toFixed(1).toString() + 'k';
+  };
+
   return (
-    <View>
-      <Text>Full name: {item.fullName}</Text>
-      <Text>Description: {item.description}</Text>
-      <Text>Language: {item.language}</Text>
-      <Text>Stars: {item.stargazersCount}</Text>
-      <Text>Forks: {item.forksCount}</Text>
-      <Text>Reviews: {item.reviewCount}</Text>
-      <Text>Rating: {item.ratingAverage}</Text>
+    <View style={styles.container}>
+      <View style={styles.userRow}>
+        <View>
+          <Image source={item.ownerAvatarUrl} style={styles.avatar} />
+        </View>
+        <View style={styles.userText}>
+          <Text style={styles.fullName}>{item.fullName}</Text>
+          <Text style={styles.description}>{item.description}</Text>
+          <Text style={styles.language}>{item.language}</Text>
+        </View>
+      </View>
+
+
+      <View style={styles.statsRow}>
+        <View style={styles.statContainer}>
+          <Text style={styles.stat}>{formatStat(item.stargazersCount)}</Text>
+          <Text style={styles.statTitle}>Stars</Text>
+        </View>
+        <View style={styles.statContainer}>
+          <Text style={styles.stat}>{formatStat(item.forksCount)}</Text>
+          <Text style={styles.statTitle}>Forks</Text>
+        </View>
+        <View style={styles.statContainer}>
+          <Text style={styles.stat}>{item.reviewCount}</Text>
+          <Text style={styles.statTitle}>Reviews</Text>
+        </View>
+        <View style={styles.statContainer}>
+          <Text style={styles.stat}>{item.ratingAverage}</Text>
+          <Text style={styles.statTitle}>Rating</Text>
+        </View>
+      </View>
     </View>
   );
 };
