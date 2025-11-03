5dce07fc8b8885d01c5596bd56661740919ef0c7
brendanharrington <brha8770@colorado.edu>
2025-11-03 16:29:59 -0700

    feat: complete exercise 10.24
    implement filter functionality by using the use-debounce library to reduce requests being made as user types

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,6 +1,8 @@
 import { FlatList, View, StyleSheet } from 'react-native';
-import { useState } from 'react';
-import {Picker} from '@react-native-picker/picker';
+import React, { useState } from 'react';
+import { Picker } from '@react-native-picker/picker';
+import { Searchbar } from 'react-native-paper';
+import { useDebounce } from 'use-debounce';
 
 import RepositoryItem from './RepositoryItem';
 import useRepositories from '../hooks/useRepositories';
@@ -17,39 +19,72 @@ const styles = StyleSheet.create({
     fontWeight: theme.fontWeights.bold,
     fontSize: theme.fontSizes.subheading,
   },
+  searchbar: {
+    backgroundColor: 'white',
+    marginBottom: 10,
+  }
 });
 
 const ItemSeparator = () => <View style={styles.separator} />;
 
-export const RepositoryListContainer = ({ repositories, choice, onChoiceChange }) => {
-  const repositoryNodes = repositories?.edges?.map(edge => edge.node) ?? [];
+export class RepositoryListContainer extends React.Component {
+  renderHeader = () => {
+  // props passed from parent
+  const { choice, onChoiceChange, filter = '', onFilterChange = () => {} } = this.props;
 
-  const renderHeader = () => (
-    <Picker 
-      style={styles.picker}
-      selectedValue={choice}
-      onValueChange={(itemValue) => onChoiceChange(itemValue)}
-    >
-      <Picker.Item label='Latest repositories' value='latest' />
-      <Picker.Item label='Highest rated repositories' value='highest' />
-      <Picker.Item label='Lowest rated repositories' value='lowest' />
-    </Picker>
-  )
-  
-  return (
-    <FlatList
-      data={repositoryNodes}
-      ItemSeparatorComponent={ItemSeparator}
-      renderItem={({ item }) => <RepositoryItem {...{ item }} />}
-      keyExtractor={item => item.id}
-      ListHeaderComponent={renderHeader}
-      showsVerticalScrollIndicator={false}
-    />
-  );
+    return (
+      <View>
+        <Searchbar
+          style={styles.searchbar}
+          placeholder='Search'
+          onChangeText={(value) => onFilterChange(value)}
+          value={filter}
+        />
+        <Picker
+          style={styles.picker}
+          selectedValue={choice}
+          onValueChange={(itemValue) => onChoiceChange(itemValue)}
+        >
+          <Picker.Item label='Latest repositories' value='latest' />
+          <Picker.Item label='Highest rated repositories' value='highest' />
+          <Picker.Item label='Lowest rated repositories' value='lowest' />
+        </Picker>
+      </View>
+    );
+  };
+
+  render() {
+    const { repositories, filter, debouncedFilter } = this.props;
+    const repositoryNodes = repositories?.edges?.map(edge => edge.node) ?? [];
+
+    // Use the debouncedFilter for list filtering (so the UI is responsive while avoiding frequent re-renders)
+    const activeFilter = (debouncedFilter !== undefined) ? debouncedFilter : (filter || '');
+
+    const filteredNodes = activeFilter
+      ? repositoryNodes.filter(node => (
+          (node.fullName || '').toLowerCase().includes(activeFilter.toLowerCase()) ||
+          (node.description || '').toLowerCase().includes(activeFilter.toLowerCase())
+        ))
+      : repositoryNodes;
+
+    return (
+      <FlatList
+        data={filteredNodes}
+        ItemSeparatorComponent={ItemSeparator}
+        renderItem={({ item }) => <RepositoryItem {...{ item }} />}
+        keyExtractor={item => item.id}
+        ListHeaderComponent={this.renderHeader}
+        showsVerticalScrollIndicator={false}
+      />
+    );
+  }
+
 }
 
 const RepositoryList = () => {
   const [choice, setChoice] = useState('latest');
+  const [filter, setFilter] = useState('');
+  const [debouncedFilter] = useDebounce(filter, 500);
 
   const vars = (() => {
     switch (choice) {
@@ -70,6 +105,9 @@ const RepositoryList = () => {
       repositories={repositories}
       choice={choice}
       onChoiceChange={setChoice}
+      filter={filter}
+      onFilterChange={setFilter}
+      debouncedFilter={debouncedFilter}
     />
   );
 };
